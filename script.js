e2371f69c9cfd9ac395fa06dcd1233d182e3c21b
mannatgupta146 <mannatgupta146@gmail.com>
2025-12-18 18:01:25 +0530

    2 done

--- script.js
+++ script.js
@@ -23,6 +23,57 @@
 // - Demonstrate at least one custom thrown error (e.g., `ExtremeTemperatureError`) and handle it in the UI.
 
 
+// function getWeather(city){
+//     let apikey = "26871e3dc9493a0fbcee6f7f70eb7bc1"
+
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`)
+
+//     .then((raw) => raw.json())
+//     .then((result) =>{
+//         console.log(result);
+        
+//     })
+// }
+
+class ExtremeTemperatureError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ExtremeTemperatureError";
+    }
+}
+
+async function getWeather(city) {
+    try {
+        let apikey = "26871e3dc9493a0fbcee6f7f70eb7bc1";
+
+        let response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+        );
+
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+
+        let result = await response.json();
+        let temp = result.main.temp;
+
+        if (temp < 0) {
+            throw new ExtremeTemperatureError("Too cold weather");
+        } 
+        else if (temp > 35) {
+            throw new ExtremeTemperatureError("Too hot weather");
+        }
+
+        console.log("City:", result.name);
+        console.log("Temperature:", temp + " °C");
+        console.log("Weather:", result.weather[0].description);
+
+    } catch (error) {
+        console.log(error.name + ":", error.message);
+    }
+}
+
+getWeather("Jammu");
 
 
 // ---
@@ -42,6 +93,50 @@
 // - Create an array of 5 mock email tasks that resolve or reject based on a random condition.
 // - Call `Promise.all` and handle success and failure cases. Show a breakdown of which emails succeeded and which failed.
 // - Ensure the `finally` block runs to update the UI or console indicating completion.
+
+const emails = [
+  "user1@gmail.com",
+  "john.doe@yahoo.com",
+  "admin@company.com",
+  "support@service.in",
+  "hello.world@outlook.com"
+];
+
+function sendEmail(email) {
+    return new Promise((resolve, reject) => {
+        let time = Math.floor(Math.random() * 10);
+
+        setTimeout(() => {
+            let probability = Math.floor(Math.random() * 10);
+            if (probability <= 5) {
+                resolve(`Accepted: ${email}`);
+            } else {
+                reject(`Rejected: ${email}`);
+            }
+        }, time * 1000);
+    });
+}
+
+function sendEmails(userList) {
+    const tasks = userList.map(email => sendEmail(email));
+
+    Promise.allSettled(tasks)
+        .then(results => {
+            results.forEach(result => {
+                if (result.status === "fulfilled") {
+                    console.log(result.value);
+                } else {
+                    console.log(result.reason);
+                }
+            });
+        })
+        .finally(() => {
+            console.log("Email process is complete.");
+        });
+}
+
+sendEmails(emails);
+
 
 // ---
 
